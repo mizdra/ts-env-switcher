28dba6371d9b7b5bbc8695421e76055687968a83
mizdra <pp.mizdra@gmail.com>
2019-10-18 02:07:36 +0900

    tsconfig.jsonをパースするところまで

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import ts from 'typescript';
 
-const configPath = ts.findConfigFile(
-  /*searchPath*/ './fixtures/src/1-minimum',
+const fixturePath = 'fixtures/src/2-include-index.ts';
+
+const configFileName = ts.findConfigFile(
+  /*searchPath*/ fixturePath,
   ts.sys.fileExists,
   'tsconfig.json',
 );
 
-console.log(configPath);
+if (!configFileName) throw new Error('tsconfig.json が見つかりません');
 
-// ts.createWatchCompilerHost;
-// ts.createCompilerHost();
+const configFile = ts.readConfigFile(configFileName, ts.sys.readFile);
+
+const parseConfigHost: ts.ParseConfigHost = {
+  fileExists: ts.sys.fileExists,
+  readFile: ts.sys.readFile,
+  readDirectory: ts.sys.readDirectory,
+  useCaseSensitiveFileNames: true,
+};
+const compilerOptions = ts.parseJsonConfigFileContent(
+  configFile.config,
+  parseConfigHost,
+  fixturePath,
+);
+
+console.log('configFileName =', configFileName);
+console.log('configFile =', configFile);
+console.log('compilerOptions =', compilerOptions);
