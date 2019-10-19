4b02e52fb7c91146563aea8065339f5c0f1f61e6
mizdra <pp.mizdra@gmail.com>
2019-10-19 17:53:05 +0900

    checkEnvとして切り出す

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,30 @@
-import ts from 'typescript';
 import { read } from './read';
 import { transform } from './transform';
 import { write } from './write';
+import { join } from 'path';
 
-const fixtureSrcPath = 'fixtures/src/2-include-index.ts';
-const fixtureDistPath = 'fixtures/dist/2-include-index.ts';
+type Option = {
+  srcBasePath: string;
+  distPath?: string;
+};
+function checkEnv(option: Option) {
+  const configFileName = join(option.srcBasePath, 'tsconfig.json');
 
-const configFileName = ts.findConfigFile(
-  /*searchPath*/ fixtureSrcPath,
-  ts.sys.fileExists,
-  'tsconfig.json',
-);
-if (!configFileName) throw new Error('tsconfig.json が見つかりません');
+  // read phase
+  const srcProject = read(option.srcBasePath, configFileName);
+  console.log(srcProject.sourceFiles.map((sourceFile) => sourceFile.fileName));
 
-// read phase
-const srcProject = read(fixtureSrcPath, configFileName);
-console.log(srcProject.sourceFiles.map((sourceFile) => sourceFile.fileName));
+  // transform phase
+  const distProject = transform(
+    srcProject,
+    option.distPath || option.srcBasePath,
+  );
 
-// transform phase
-const distProject = transform(srcProject, fixtureDistPath);
+  // write phase
+  if (option.distPath) write(distProject);
+}
 
-// write phase
-write(distProject);
+checkEnv({
+  srcBasePath: 'fixtures/src/2-include-index.ts',
+  distPath: 'fixtures/dist/2-include-index.ts',
+});
