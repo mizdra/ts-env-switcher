f383785ed9da2566d8e2bf03bf2fa5a28a0c33a7
mizdra <pp.mizdra@gmail.com>
2019-11-01 01:01:55 +0900

    snapshot取ってみたけど巨大すぎたので諦める

---
+++ test/snapshots.ts
@@ -0,0 +1,12 @@
+// import { read } from '../src/read';
+// import { transform } from '../src/transform';
+// import { resolve } from 'path';
+
+// test('adds 1 + 2 to equal 3', () => {
+//   const srcBasePath = resolve('fixtures/src/6-external-library');
+//   const configFileName = resolve(srcBasePath, 'tsconfig.json');
+//   const distPath = resolve('fixtures/dist/6-external-library');
+//   const srcProject = read(srcBasePath, configFileName);
+//   const distProject = transform(srcProject, distPath || srcBasePath);
+//   expect(distProject).toMatchSnapshot();
+// });
