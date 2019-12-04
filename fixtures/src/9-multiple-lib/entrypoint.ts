d90904b06819ed446a4799b4e5442ffe0fa16fa5
mizdra <pp.mizdra@gmail.com>
2019-12-05 00:37:24 +0900

    テストケースを追加

---
+++ fixtures/src/9-multiple-lib/entrypoint.ts
@@ -0,0 +1,8 @@
+// switch: { "lib": ["dom", "webworker"] }
+function fnA() {}
+
+// switch: { "lib": ["dom", "es5"] }
+function fnB() {}
+
+// switch: { "lib": ["webworker", "dom"] }
+function fnC() {}
