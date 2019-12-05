70e3fe83a47b24627af931ded2d1b5e25223c903
mizdra <pp.mizdra@gmail.com>
2019-12-05 17:11:06 +0900

    wip: bodyの削除をサポート

--- src/transform.ts
+++ src/transform.ts
@@ -3,11 +3,7 @@ import { join, relative } from 'path';
 import { isSubDirectory } from './lib/path';
 import ts from 'typescript';
 
-function transformPath(
-  srcBasePath: string,
-  distBasePath: string,
-  srcFileName: string,
-) {
+function transformPath(srcBasePath: string, distBasePath: string, srcFileName: string) {
   if (isSubDirectory(srcBasePath, srcFileName)) {
     return join(distBasePath, relative(srcBasePath, srcFileName));
   }
@@ -15,15 +11,72 @@ function transformPath(
   return srcFileName;
 }
 
-function pathTransformerFactory(
-  srcBasePath: string,
-  distBasePath: string,
-): ts.TransformerFactory<ts.SourceFile> {
+function createEmptyBody() {
+  return ts.createBlock(
+    [
+      ts.createReturn(
+        ts.createAsExpression(ts.createNumericLiteral('0'), ts.createKeywordTypeNode(ts.SyntaxKind.NeverKeyword)),
+      ),
+    ],
+    true,
+  );
+}
+
+function pathTransformerFactory(srcBasePath: string, distBasePath: string): ts.TransformerFactory<ts.SourceFile> {
   return (context: ts.TransformationContext) => (sourceFile: ts.SourceFile) => {
     return {
       ...sourceFile,
       fileName: transformPath(srcBasePath, distBasePath, sourceFile.fileName),
     };
+  };
+}
+
+function deleteBodyTransformerFactory(directive: SwitchDirective): ts.TransformerFactory<ts.SourceFile> {
+  return (context: ts.TransformationContext) => (sourceFile: ts.SourceFile) => {
+    function visit(node: ts.Node): ts.Node {
+      // NOTE: `node.body` は `undefined` の可能性があるので undefined チェックする
+      if (ts.isFunctionDeclaration(node) && node.body) {
+        node = ts.updateFunctionDeclaration(
+          node,
+          node.decorators,
+          node.modifiers,
+          node.asteriskToken,
+          node.name,
+          node.typeParameters,
+          node.parameters,
+          node.type,
+          createEmptyBody(),
+        );
+      }
+
+      if (ts.isFunctionExpression(node) && node.body) {
+        node = ts.updateFunctionExpression(
+          node,
+          node.modifiers,
+          node.asteriskToken,
+          node.name,
+          node.typeParameters,
+          node.parameters,
+          node.type,
+          createEmptyBody(),
+        );
+      }
+
+      if (ts.isArrowFunction(node) && node.body) {
+        node = ts.updateArrowFunction(
+          node,
+          node.modifiers,
+          node.typeParameters,
+          node.parameters,
+          node.type,
+          node.equalsGreaterThanToken,
+          createEmptyBody(),
+        );
+      }
+
+      return ts.visitEachChild(node, visit, context);
+    }
+    return ts.visitNode(sourceFile, visit);
   };
 }
 
@@ -33,39 +86,27 @@ export type TransformOption = {
 };
 
 export function transform(
-  {
-    basePath: srcBasePath,
-    config: srcConfig,
-    packages: srcPackages,
-    sourceFiles: srcSourceFiles,
-  }: Project,
+  { basePath: srcBasePath, config: srcConfig, packages: srcPackages, sourceFiles: srcSourceFiles }: Project,
   transformOption: TransformOption,
 ): Project {
   const distConfig: Project['config'] = {
-    fileName: transformPath(
-      srcBasePath,
-      transformOption.distBasePath,
-      srcConfig.fileName,
-    ),
+    fileName: transformPath(srcBasePath, transformOption.distBasePath, srcConfig.fileName),
     compilerOptions: {
       ...srcConfig.compilerOptions,
       // lib をディレクティブで指定されたもので上書きする
-      lib: (transformOption.directive.lib ?? []).map(
-        (libName) => `lib.${libName}.d.ts`,
-      ),
+      lib: (transformOption.directive.lib ?? []).map((libName) => `lib.${libName}.d.ts`),
     },
   };
   const distPackages = srcPackages.map((sourceFile) => ({
     ...sourceFile,
-    fileName: transformPath(
-      srcBasePath,
-      transformOption.distBasePath,
-      sourceFile.fileName,
-    ),
+    fileName: transformPath(srcBasePath, transformOption.distBasePath, sourceFile.fileName),
   }));
   const transformationResult = ts.transform(
     srcSourceFiles.map((sourceFile) => ts.getMutableClone(sourceFile)),
-    [pathTransformerFactory(srcBasePath, transformOption.distBasePath)],
+    [
+      pathTransformerFactory(srcBasePath, transformOption.distBasePath),
+      deleteBodyTransformerFactory(transformOption.directive),
+    ],
   );
   const distSourceFiles = transformationResult.transformed;
 
