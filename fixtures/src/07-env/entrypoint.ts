6e1dffd16c3de4076cb035c13a82f18abb07a3d7
mizdra <pp.mizdra@gmail.com>
2019-12-07 21:42:11 +0900

    メソッド定義のテストケースを追加

--- fixtures/src/07-env/entrypoint.ts
+++ fixtures/src/07-env/entrypoint.ts
@@ -4,3 +4,8 @@ function fnA() {}
 const fnB = /* switch: { "lib": ["es5"] } */ function() {};
 
 const fnC = /* switch: { "lib": ["ESNext"] } */ () => {};
+
+const obj = {
+  /* switch: { "lib": ["ESNext"] } */
+  fcD() {},
+};
