ae269dacf88eb58d9f3e625b20fa0b79439883d1
mizdra <pp.mizdra@gmail.com>
2019-12-08 16:22:20 +0900

    writeフェーズをスキップできるように

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
 import { read } from './read';
 import { transform } from './transform';
 import { write } from './write';
-import { join } from 'path';
+import { join, basename } from 'path';
 import { collectDirectives } from './collect';
 import { debug, format } from './lib/logger';
 import { createDirectiveIdentifier } from './lib/directive';
 import { check } from './check';
+import { getConfigFileName } from './lib/path';
 
 type Option = {
-  srcBasePath: string;
-  distBasePath: string;
+  project: string;
+  outDir?: string;
 };
 
 export function checkEnv(option: Option) {
-  const configFileName = join(option.srcBasePath, 'tsconfig.json');
+  const configFileName = getConfigFileName(option.project);
 
   // read phase
-  const srcProject = read(option.srcBasePath, configFileName);
+  const srcProject = read(option.project, configFileName);
   debug(format(srcProject.sourceFiles.map((sourceFile) => sourceFile.fileName)));
 
   // collect phase
@@ -24,13 +25,15 @@ export function checkEnv(option: Option) {
 
   for (const directive of directives) {
     debug(format(directive));
-    const distBasePath = join(option.distBasePath, createDirectiveIdentifier(directive));
 
     // transform phase
     const distProject = transform(srcProject, directive);
 
-    // write phase
-    write(distProject, distBasePath);
+    // write phase (--outDir が指定された時のみ)
+    if (option.outDir !== undefined) {
+      const distBasePath = join(option.outDir, createDirectiveIdentifier(directive));
+      write(distProject, distBasePath);
+    }
 
     check(distProject, directive);
   }
--- src/lib/path.ts
+++ src/lib/path.ts
@@ -1,5 +1,10 @@
-import { relative } from 'path';
+import { relative, basename, join } from 'path';
 
 export function isSubDirectory(parent: string, child: string) {
   return !relative(parent, child).startsWith('..');
+}
+
+export function getConfigFileName(project: string): string {
+  if (basename(project) === 'tsconfig.json') return project;
+  return join(project, 'tsconfig.json');
 }
