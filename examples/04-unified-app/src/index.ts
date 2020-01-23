7d3db996debb30946f03552dbf47d33414d02c17
mizdra <pp.mizdra@gmail.com>
2020-01-23 17:40:04 +0900

    update

---
+++ examples/04-unified-app/src/index.ts
@@ -0,0 +1,22 @@
+function isBrowser() {
+  return 'window' in globalThis;
+}
+
+function isNodeJS() {
+  return 'process' in globalThis;
+}
+
+function log(message: string) {
+  const formattedMessage = Date.now() + ': ' + message;
+  /* switch: { "-types": ["node"] } */
+  if (isBrowser()) {
+    document.write(formattedMessage);
+  }
+  /* switch: { "-lib": ["dom"] } */
+  if (isNodeJS()) {
+    document.write(formattedMessage); // error
+    // process.stdout.write(message);
+  }
+}
+
+log('message');
